2a8e0b32c716024efccf52f924baf668458a9630
at337am <lueqit@gmail.com>
2025-06-18 03:32:02 +0800

    feat(gostart): 将 logutil 改为 fmtutil, 并删除了时间显示函数

--- gostart/main.go
+++ gostart/main.go
@@ -5,7 +5,7 @@ import (
 	"embed"
 	"flag"
 	"fmt"
-	"gostart/pkg/logutil"
+	"gostart/pkg/fmtutil"
 	"io/fs"
 	"os"
 	"os/exec"
@@ -25,7 +25,7 @@ func main() {
 
 	args := flag.Args()
 	if len(args) != 1 {
-		logutil.PrintError("请提供且只提供一个项目名称参数")
+		fmtutil.PrintError("请提供且只提供一个项目名称参数")
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -33,25 +33,25 @@ func main() {
 
 	for _, r := range projectDir {
 		if !unicode.IsLetter(r) && r != '_' {
-			logutil.PrintError("项目名称应当使用字母 (a-z, A-Z) 和下划线 (_)")
+			fmtutil.PrintError("项目名称应当使用字母 (a-z, A-Z) 和下划线 (_)")
 			os.Exit(1)
 		}
 	}
 
 	// 检查路径是否存在, 如果已经存在则提示失败
 	if _, err := os.Stat(projectDir); err == nil {
-		logutil.PrintError("目标目录或文件已存在. 请删除现有内容或选择其他项目名称.")
+		fmtutil.PrintError("目标目录或文件已存在. 请删除现有内容或选择其他项目名称.")
 		os.Exit(1)
 	} else if !os.IsNotExist(err) {
-		logutil.PrintError(fmt.Sprintf("检查目标路径 [%s] 失败: %v\n", projectDir, err))
+		fmtutil.PrintError(fmt.Sprintf("检查目标路径 [%s] 失败: %v\n", projectDir, err))
 		os.Exit(1)
 	}
 
-	logutil.PrintInfo("正在生成项目...")
+	fmtutil.PrintInfo("正在生成项目...")
 
 	// 创建项目目录
 	if err := os.MkdirAll(projectDir, 0755); err != nil {
-		logutil.PrintError(fmt.Sprintf("创建项目目录 [%s] 失败: %v", projectDir, err))
+		fmtutil.PrintError(fmt.Sprintf("创建项目目录 [%s] 失败: %v", projectDir, err))
 		os.Exit(1)
 	}
 
@@ -83,7 +83,7 @@ func main() {
 		}
 		return nil
 	}); err != nil {
-		logutil.PrintError(fmt.Sprintf("复制模板文件失败: %v", err))
+		fmtutil.PrintError(fmt.Sprintf("复制模板文件失败: %v", err))
 		os.Exit(1)
 	}
 
@@ -95,7 +95,7 @@ func main() {
 	cmd.Stderr = &stderr
 
 	if err := cmd.Run(); err != nil {
-		logutil.PrintError(fmt.Sprintf("执行 go mod init %s 失败: %v", projectDir, err))
+		fmtutil.PrintError(fmt.Sprintf("执行 go mod init %s 失败: %v", projectDir, err))
 		os.Exit(1)
 	}
 
@@ -105,11 +105,9 @@ func main() {
 	tidyCmd.Stderr = &tidyStderr
 
 	if err := tidyCmd.Run(); err != nil {
-		logutil.PrintWarning(fmt.Sprintf("执行 go mod tidy 失败: %v\n%s", err, tidyStderr.String()))
-	} else {
-		logutil.PrintInfo("已执行 go mod tidy")
+		fmtutil.PrintWarning(fmt.Sprintf("执行 go mod tidy 失败: %v\n%s", err, tidyStderr.String()))
 	}
 
 	// todo, 使用交互选择, 创建不同模板的项目
-	logutil.PrintSuccess(fmt.Sprintf("[%s] 创建成功!", projectDir))
+	fmtutil.PrintSuccess(fmt.Sprintf("[%s] 创建成功!", projectDir))
 }
