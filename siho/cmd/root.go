7cd03c94477af69d825c4d6fe45ee446df022a19
at337am <lueqit@gmail.com>
2025-07-29 02:33:09 +0800

    feat: 增加 -o 选项

--- siho/cmd/root.go
+++ siho/cmd/root.go
@@ -46,7 +46,7 @@ func newRootCmd() *cobra.Command {
 		},
 	}
 
-	cmd.Flags().StringVarP(&runner.OutputDir, "output-dir", "", "", "Specify the directory path to store the output results")
+	cmd.Flags().StringVarP(&runner.OutputDir, "output-dir", "o", "", "Specify the directory path to store the output results")
 	cmd.Flags().BoolVarP(&runner.Decrypt, "decrypt", "d", false, "Enable decryption mode to restore encrypted files")
 
 	return cmd
